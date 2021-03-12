168a7aa1720b4b66b32505ee47ae3fb46d930ff1
Aaron <aaronjunehee@gmail.com>
2021-03-12 14:30:03 -0500

    creating shared header component

---
+++ src/components/Registration/Header.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <header className='login-header'>
+      <h1>FitTrack 🏋️‍♂️</h1>
+      <p>A simple app that helps <span className="line-break">you keep track of your training</span></p>
+    </header>
+  )
+}
+
+export default Header
