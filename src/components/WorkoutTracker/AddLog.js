1e48e1c1864767c7963ed462bbc04c629dc6eed6
Aaron <aaronjunehee@gmail.com>
2021-03-17 12:01:19 -0400

    making this component a controlled component and adding accessibility features

--- src/components/WorkoutTracker/AddLog.js
+++ src/components/WorkoutTracker/AddLog.js
@@ -1,4 +1,4 @@
-import React, { useState, useRef } from 'react'
+import React, { useState } from 'react'
 import Button from '@material-ui/core/Button'
 
 const newExercise = { name: '', sets: 0, reps: 0, weight: 0, unit: 'lbs' }
@@ -6,7 +6,6 @@ const newExercise = { name: '', sets: 0, reps: 0, weight: 0, unit: 'lbs' }
 function AddLog(props) {
   const [exercises, setExercises] = useState([{...newExercise}])
   const [error, setError] = useState('')
-  const formEl = useRef(null)
 
   const addExerciseRow = () => {
     const exerciseToAdd = [...exercises]
@@ -23,7 +22,8 @@ function AddLog(props) {
 
   const updateExerciseField = (e, i) => {
     const exerciseState = [...exercises]
-    exerciseState[i][e.target.name] = e.target.value
+    const name = e.target.name.replace(/-\d+/g, '')
+    exerciseState[i][name] = e.target.value
     setExercises(exerciseState)
     setError('')
   }
@@ -59,7 +59,6 @@ function AddLog(props) {
 
   const resetVars = () => {
     setExercises([{ ...newExercise }])
-    formEl.current.reset()
     setError('')
   }
 
@@ -74,32 +73,33 @@ function AddLog(props) {
   }
 
   return (
-    <form className="add-log" ref={formEl} onSubmit={saveExercises}>
-      {exercises.map((exercise, i) => {
+    <form className="add-log" onSubmit={saveExercises}>
+      {exercises.map((ex, i) => {
         return (
-          <fieldset onChange={(e) => updateExerciseField(e, i)} key={i} className={exercises.length > 1 ? 'container border-bottom' : 'container'}>
+          <fieldset key={i} className={exercises.length > 1 ? 'container border-bottom' : 'container'}>
             { i > 0 && <i onClick={(e) => deleteRow(e, i)} className="fas fa-times-circle"></i> }
             <div className="input-container">
-              <label htmlFor="name"><p>Name</p></label>
-              <input type="text" name="name" id="name" defaultValue={exercise.name} />
+              <label htmlFor={`name-${i}`}><p>Name</p></label>
+              <input type="text" name={`name-${i}`} id={`name-${i}`} value={exercises[i].name} onChange={(e) => updateExerciseField(e, i)} />
             </div>
             <div className="input-container">
-              <label htmlFor="weight"><p>Weight</p></label>
-              <input type="number" name="weight" id="weight" defaultValue={exercise.weight} />
+              <label htmlFor={`weight-${i}`}><p>Weight</p></label>
+              <input type="number" name={`weight-${i}`} id={`weight-${i}`} value={exercises[i].weight} onChange={(e) => updateExerciseField(e, i)} />
               <div className="select-container">
-                <select name="unit" id="unit" defaultValue={exercise.unit}>
+                <label htmlFor={`unit-${i}`} className="visuallyhidden">Unit of Measurement</label>
+                <select name={`unit-${i}`} id={`unit-${i}`} value={exercises[i].unit} onChange={(e) => updateExerciseField(e, i)}>
                   <option value="lbs">lbs</option>
                   <option value="kg">kg</option>
                 </select>
               </div>
             </div>
             <div className="input-container">
-              <label htmlFor="sets"><p>Sets</p></label>
-              <input type="number" name="sets" id="sets" defaultValue={exercise.sets}/>
+              <label htmlFor={`sets-${i}`}><p>Sets</p></label>
+              <input type="number" name={`sets-${i}`} id={`sets-${i}`} value={exercises[i].sets} onChange={(e) => updateExerciseField(e, i)} />
             </div>
             <div className="input-container">
-              <label htmlFor="reps"><p>Reps</p></label>
-              <input type="number" name="reps" id="reps" defaultValue={exercise.reps}/>
+              <label htmlFor={`reps-${i}`}><p>Reps</p></label>
+              <input type="number" name={`reps-${i}`} id={`reps-${i}`} value={exercises[i].reps} onChange={(e) => updateExerciseField(e, i)} />
             </div>
           </fieldset>
         )
